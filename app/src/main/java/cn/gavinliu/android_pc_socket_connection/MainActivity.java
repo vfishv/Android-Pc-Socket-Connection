0055b6579ea51dfe30aa5635c846b350ff6aa57e
Quentin <513450035@qq.com>
2019-10-16 16:13:42 +0800

    androidx

--- app/src/main/java/cn/gavinliu/android_pc_socket_connection/MainActivity.java
+++ app/src/main/java/cn/gavinliu/android_pc_socket_connection/MainActivity.java
@@ -3,7 +3,7 @@ package cn.gavinliu.android_pc_socket_connection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 import android.util.Log;
 
